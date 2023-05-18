a7217fb522be4633fd269e2caa1cd2fb44e5d18e
antoninosartori <sartoriantonino@gmail.com>
2023-05-18 10:44:26 -0300

    validaciones form

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,21 @@ function App() {
     event.preventDefault();
 
     // verificar
-    
+    const name = form.current.name.value
+    const email = form.current.email.value
+    const message = form.current.message.value
+
+    if(!name || name.length < 3){
+      setMessageResults('Por favor, escribe un nombre válido')
+      return
+    }
+
+    if(!message || message.length < 5){
+      setMessageResults('Por favor, escribe un mensaje válido')
+      return 
+    }
+
+    setMessageResults('')
     setIsDisabled(true)
     emailjs.sendForm('service_rqdt8w9', 'template_2dk062d', form.current, '1BLpRCJNGW4edyOyK')
       .then((result) => {
@@ -40,7 +54,7 @@ function App() {
           setIsDisabled(false)
           setMessageResults('mensaje enviado correctamente')
           form.current.reset()
-          setInterval(() => setMessageResults(''), 3000)
+          setTimeout(() => setMessageResults(''), 3000)
       }, (error) => {
           console.log(error.text);
           setMessageResults('lo siento, no pudimos enviar su mensaje')
@@ -181,9 +195,9 @@ function App() {
           <form ref={form} onSubmit={sendEmail} className='contactForm'>
             <p>Los productos publicados son originales, cerrados y con su sello de importación.</p>
             <p>¿Tenés alguna pregunta o querés encargar algún producto?</p>
-            <input className='contactForm-input' type="text" placeholder='Escribe tu nombre' name='name' />
-            <input className='contactForm-input' type="email" placeholder='Escribe tu email' name='email' />
-            <textarea className='contactForm-input' id="" cols="30" rows="10" placeholder='Escibe tu mensaje' name='message'></textarea>
+            <input required className='contactForm-input' type="text" placeholder='Escribe tu nombre' name='name' />
+            <input required className='contactForm-input' type="email" placeholder='Escribe tu email' name='email' />
+            <textarea required className='contactForm-input' id="" cols="30" rows="10" placeholder='Escibe tu mensaje' name='message'></textarea>
             <button disabled={isDisabled} className='contactForm-sendButton'>enviar</button>
             {isDisabled && <LoadingSpinner/> }
             {messageResult && <p className='messageStatus'>{messageResult}</p>}
