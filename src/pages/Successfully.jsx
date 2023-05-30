6b391d2df454c7dc2daf8748972d2b0de01762fa
antoninosartori <sartoriantonino@gmail.com>
2023-05-30 14:48:20 -0300

    seccion successfully con pasos a seguir para la transferencia

--- src/pages/Successfully.jsx
+++ src/pages/Successfully.jsx
@@ -22,17 +22,48 @@ const Successfully = () => {
                 <img className='smile' src={smile} alt="carita sonriente" />
                 <h2>Gracias</h2>
                 <h3>Hemos recibido tu pedido</h3>
-                <h3 className='total'>total: <span>${calculateTotal()}</span></h3>
-                <p>Estos son los datos para que puedas depositar</p>
-                <article className='mercadoPago-container'>
-                    {
-                        mercadoPagoInfo.map(item => (
-                            < TextToClipBoard key={item.title} textToTitle={item.title} textToCopy={item.info} />
-                        ))
-                    }
-                </article>
-                <p>Recordá que te guardamos el stock por 24 horas</p>
-                <p>Ponete en contacto con nosótros, podes hacerlo por cualquier medio</p>
+
+                <div className='successfully-paymentContainer'>
+                    <div className='successfully-payment'>
+                        <p className='title-article'>Estos son los datos para que puedas depositar</p>
+                        <article className='mercadoPago-container'>
+                            {
+                                mercadoPagoInfo.map(item => (
+                                    < TextToClipBoard key={item.title} textToTitle={item.title} textToCopy={item.info} isCopiable={item.isCopiable}  />
+                                ))
+                            }
+                        </article>
+                    </div>
+                    <div className='payment-footsprings'>
+                        <p className='title-article'>Pasos a seguir</p>
+                        <div className='footspring-group'>
+                            <span className='footspring-number'>1</span>
+                            <p className='footspring-text'>Realizá la transferencia.</p>
+                        </div>
+                        <div className='footspring-group'>
+                            <span className='footspring-number'>2</span>
+                            <p className='footspring-text'>Guardá el comprobante y mandalo por whatsapp o email.</p>
+                        </div>
+                        <div className='footspring-group'>
+                            <span className='footspring-number'>3</span>
+                            <p className='footspring-text'>Te vamos a contactar para coordinar la venta.</p>
+                        </div>
+                        <div className='footspring-group'>
+                            <span className='footspring-number'>💡</span>
+                            <p className='footspring-text'>Usando Mercado Pago podés pagar en cuotas.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='checkout-payment__footer'>
+                    <h3 className='total-cart'>Total a pagar: <span>${calculateTotal()}</span> </h3>
+                    <div className='payment-buttons'>
+                        <Link to='/' className='contactForm-sendButton goBackCart'>volver atras</Link>
+                        <Link to='/buy' className='contactForm-sendButton goOnButton'>continuar</Link>
+                    </div>
+                </div>
+                <p className='successfully-footer-text'>Recordá que te guardamos el stock por 24 horas</p>
+                <p className='successfully-footer-text'>Ponete en contacto con nosótros, podes hacerlo por cualquier medio</p>
                 <div className='successfully-socials'>
                     <a className='' href={`https://wa.me/543446544456?text=${whatsappMessage}`} target='_blank' rel='noreferrer'>
                         <img className='whatsapp-image' src={whatsapp} alt="Enviar mensaje de texto con ese mensaje" />
