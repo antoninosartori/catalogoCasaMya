733133944c8d3ff4640cce75f3cefd35e5ff20f0
antoninosartori <sartoriantonino@gmail.com>
2023-08-31 09:12:02 -0300

    perfumes agotados no se muestran

--- src/App.jsx
+++ src/App.jsx
@@ -16,43 +16,42 @@ import Order from './pages/Order'
 
 
 function App() {
-  const { setFragances, setFilteredFragances, setIsLoading, setError } = useContext(appContext)
+   const { setFragances, setFilteredFragances, setIsLoading, setError } = useContext(appContext)
 
-  useEffect(() => {
-    // fetch
-    setIsLoading(true)
-    fetch(URL_API)
-      .then((response) => response.json())
-      .then((data) => {
-        const sortedData = data.sort((a,b) => b.precio - a.precio)
-        const normalFragances = sortedData.filter(prod => !prod.esEncargable)
-        setFilteredFragances(normalFragances);
-        setFragances(sortedData);
-        setIsLoading(false)
-      })
-      .catch((error) => {
-        setIsLoading(false)
-        setError(true)
-        console.error(error);
-      });
-  }, [])
+   useEffect(() => {
+      // fetch
+      setIsLoading(true)
+      fetch(URL_API)
+         .then((response) => response.json())
+         .then((data) => {
+            const sortedData = data.sort((a, b) => b.precio - a.precio)
+            const normalFragances = sortedData.filter(prod => !prod.esEncargable && prod.estado !== 'agotado')
+            setFilteredFragances(normalFragances);
+            setFragances(sortedData);
+         })
+         .catch((error) => {
+            setError(true)
+            console.error(error);
+         })
+         .finally(() => setIsLoading(false))
+   }, [])
 
-  return (
-    <>
-      {/* < Header /> */}
+   return (
+      <>
+         {/* < Header /> */}
 
-      <Routes>
-        < Route path='/' element={ < HomePage /> } />
-        < Route path='/checkout' element={ < Checkout /> } />
-        < Route path='/buy' element={ < CompleteBuy /> } />
-        < Route path='/successfully' element={ < Successfully /> } />
-        {/* < Route path='/encargar' element={ < Order /> } /> */}
-      </Routes>
+         <Routes>
+            < Route path='/' element={< HomePage />} />
+            < Route path='/checkout' element={< Checkout />} />
+            < Route path='/buy' element={< CompleteBuy />} />
+            < Route path='/successfully' element={< Successfully />} />
+            {/* < Route path='/encargar' element={ < Order /> } /> */}
+         </Routes>
 
 
-      < Footer />
-    </>
-  )
+         < Footer />
+      </>
+   )
 }
 
 export default App
--- src/context/StateContextApp.jsx
+++ src/context/StateContextApp.jsx
@@ -1,66 +1,67 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { appContext } from './appContext'
 
-const StateContextApp = ({children}) => {
-    const [fragances, setFragances] = useState([])
-    const [filteredFragances, setFilteredFragances] = useState([])
-    const [search, setSearch] = useState('')
-    const [title, setTitle] = useState('Catálogo')
-    const [cart, setCart ] = useState([])
-    const [ openCart, setOpenCart] = useState(false)
-    const [isLoading, setIsLoading] = useState(false)
-    const [error, setError] = useState('')
+const StateContextApp = ({ children }) => {
+   const [fragances, setFragances] = useState([])
+   const [filteredFragances, setFilteredFragances] = useState([])
+   const [search, setSearch] = useState('')
+   const [title, setTitle] = useState('Catálogo')
+   const [cart, setCart] = useState([])
+   const [openCart, setOpenCart] = useState(false)
+   const [isLoading, setIsLoading] = useState(false)
+   const [error, setError] = useState('')
 
-    const filterSearch = (searchValue) => {
-        /* if(searchValue === '') {
-            setFilteredFragances(fragances.filter(prod => !prod.esEncargable))
-        } */
-        const filter = fragances.filter(fragance => fragance.nombre.includes(searchValue) || fragance.marca.includes(searchValue) || fragance.genero.includes(searchValue))
-        if(filter.length === 0){ 
-            setTitle(`No se han encontrado productos`) 
-        }
-        setFilteredFragances(filter)
-    }
+   const filterSearch = (searchValue) => {
+      /* if(searchValue === '') {
+          setFilteredFragances(fragances.filter(prod => !prod.esEncargable))
+      } */
+      const filter = fragances.filter(fragance => fragance.nombre.includes(searchValue) || fragance.marca.includes(searchValue) || fragance.genero.includes(searchValue))
+      if (filter.length === 0) {
+         setTitle(`No se han encontrado productos`)
+      }
+      setFilteredFragances(filter)
+   }
 
-    const resetSearch = (event) => {
-        event.preventDefault()
-        setFilteredFragances(fragances.filter(prod => !prod.esEncargable))
-        setSearch('')
-        setTitle('Catálogo')
-    }
+   const resetSearch = (event) => {
+      event.preventDefault()
+      setFilteredFragances(fragances.filter(prod => !prod.esEncargable))
+      setSearch('')
+      setTitle('Catálogo')
+   }
 
-    const calculateTotal = () => {
-        const totalPrice = cart.reduce((total, item) => total + Number(item.precio), 0);
-        /* return totalPrice.toFixed(2) */ // Redondear a 2 decimales si es necesario
-        return totalPrice.toLocaleString()
-      };
+   const calculateTotal = () => {
+      const totalPrice = cart.reduce((total, item) => total + Number(item.precio), 0);
+      /* return totalPrice.toFixed(2) */ // Redondear a 2 decimales si es necesario
+      return totalPrice.toLocaleString()
+   };
 
-    return(
-        <appContext.Provider
-            value={{
-                fragances, 
-                setFragances, 
-                filteredFragances, 
-                setFilteredFragances, 
-                search, 
-                setSearch, 
-                title, 
-                setTitle, 
-                filterSearch, 
-                resetSearch, 
-                cart, 
-                setCart, 
-                openCart, 
-                setOpenCart, 
-                isLoading, 
-                setIsLoading, 
-                error, 
-                setError, 
-                calculateTotal}}
-        >
-            {children}
-        </appContext.Provider>
-    )
+   return (
+      <appContext.Provider
+         value={{
+            fragances,
+            setFragances,
+            filteredFragances,
+            setFilteredFragances,
+            search,
+            setSearch,
+            title,
+            setTitle,
+            filterSearch,
+            resetSearch,
+            cart,
+            setCart,
+            openCart,
+            setOpenCart,
+            isLoading,
+            setIsLoading,
+            error,
+            setError,
+            calculateTotal
+         }}
+      >
+         {children}
+      </appContext.Provider>
+   )
 }
 
 export default StateContextApp
